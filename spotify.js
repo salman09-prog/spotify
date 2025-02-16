3c2bbb48111165fad6e98de7409a61e13eb49188
Salman yusuf zai <salmanyusufzai48@gmail.com>
2025-02-16 22:25:06 +0530

    changes made

--- spotify.js
+++ spotify.js
@@ -24,7 +24,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
   currFolder = folder;
   
-  let a = await fetch(`http://127.0.0.1:5500/${currFolder}/`);
+  let a = await fetch(`https://salman09-prog.github.io/spotify/${currFolder}/`);
   let response = await a.text();
   //For parsing only songs not whole data
   let div = document.createElement("div");
@@ -99,7 +99,7 @@ function playMusic(track, pause = false) {
 //For displaying albums
 
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:5500/songs/`);
+  let a = await fetch(`https://salman09-prog.github.io/spotify/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
