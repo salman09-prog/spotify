54adf89d35b0b2f9964753979cc0c665099250c8
Salman yusuf zai <salmanyusufzai48@gmail.com>
2025-02-16 22:09:03 +0530

    changes made

--- spotify.js
+++ spotify.js
@@ -99,7 +99,7 @@ function playMusic(track, pause = false) {
 //For displaying albums
 
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:5501/songs/`);
+  let a = await fetch(`http://127.0.0.1:5500/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -115,7 +115,7 @@ async function displayAlbums() {
 
       //Get metadata of the folder
 
-      let a = await fetch(`http://127.0.0.1:5501/songs/${folder}/info.json`);
+      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
       let response = await a.json();
       let cardContainer = document.querySelector(".cardContainer")
       cardContainer.innerHTML = cardContainer.innerHTML + `   <div data-folder="${folder}" class="card">
